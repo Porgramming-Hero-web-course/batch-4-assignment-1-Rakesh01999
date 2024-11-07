10d0046135a2764e3bb4fc151361c9456938d7dc
Rakesh Biswas <rbiswas01999@gmail.com>
2024-11-07 14:05:46 +0600

    problem-7 added

--- src/problem-6.ts
+++ src/problem-6.ts
@@ -1,13 +1,12 @@
 
-interface Profile{
+interface Profile {
     name: string;
     age: number;
     email: string;
-  }
-  function updateProfile(profile: Profile, updates: Partial<Profile>): Profile {
+}
+function updateProfile(profile: Profile, updates: Partial<Profile>): Profile {
     return { ...profile, ...updates };
-  }
-  
+}
+
 const myProfile: Profile = { name: "Alice", age: 25, email: "alice@example.com" };
 console.log(updateProfile(myProfile, { age: 26 }));
-  
