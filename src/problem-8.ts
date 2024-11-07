37f143f8d4c8216b4ea9341d4da2e85ed1f6c5aa
Rakesh Biswas <rbiswas01999@gmail.com>
2024-11-07 14:14:22 +0600

    problem-8 added

---
+++ src/problem-8.ts
@@ -0,0 +1,14 @@
+{
+
+    const validateKeys = <T extends object>(obj: T, keys: (keyof T)[]): boolean => {
+        return keys.every(key => key in obj);
+    };
+
+    // Sample Input
+    const person = { name: "Alice", age: 25, email: "alice@example.com" };
+    
+    console.log(validateKeys(person, ["name", "age"])); // Output: true
+
+    // console.log(validateKeys(person, ["name", "address"])); // shows warning and here Output: false
+
+}
